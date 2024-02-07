cf59c34e97c8f92f50ad02252a86804a4429adb2
hafeez100 <88046804+hafeez100@users.noreply.github.com>
2024-02-07 10:29:58 +0000

    datatype

---
+++ .devcontainer/01_basic/conversion.js
@@ -0,0 +1,13 @@
+// let score="33AA"
+// console.log(typeof score)
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+// let isLoggedIn="gfhggf"
+// let BooleanisLoggedIn=Boolean(isLoggedIn)
+// console.log(BooleanisLoggedIn)
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
---
+++ .devcontainer/01_basic/variable.js
@@ -0,0 +1,10 @@
+const accountId=144553
+let accountEmil="djfsgjf"
+var accountPassword="12334"
+accountCity="jaipur"
+
+accountEmail="dddddd"
+accountPassword="1111"
+accountCity="Banglore"
+
+console.table([accountEmil,accountCity,accountId,accountPassword])
